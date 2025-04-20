4e24467bd9d472a381385434af4f1be48c9b076c
gmartpad <contato.gabrielmartinspadoin@gmail.com>
2025-04-20 17:07:48 -0300

    [NG] TRANSFORMING_AND_USING_RESPONSE_DATA - feat: transforming the response from the request and getting only the places prop from the response via rxjs map and subscription pipe

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -4,6 +4,7 @@ import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-available-places',
@@ -20,9 +21,15 @@ export class AvailablePlacesComponent implements OnInit {
   ngOnInit() {
     const subscription = this.httpClient
       .get<{ places: Place[] }>('http://localhost:3000/places')
+      .pipe(
+        map((res) => res.places)
+      )
       .subscribe({
-        next: (res) => {
-          console.log(res)
+        next: (places) => {
+          console.log(places)
+          if(places?.length) {
+            this.places.set(places)
+          }
         }
       })
 
